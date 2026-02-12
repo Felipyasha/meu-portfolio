5753e3ab2e1f9b2f7117ae068964412e89253219
Felipyasha <felipe_nascalves@hotmail.com>
2026-02-12 15:12:49 -0300

    fix: rendering fix for GitHub Pages

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,10 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'project/:id',
+    renderMode: RenderMode.Client // Isso diz ao Angular: "Não tente gerar isso no build, deixe para o navegador"
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
